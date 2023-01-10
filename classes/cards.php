b31adfcb2f98f0e10ba0d7992a3f43e2f31487c8
alon-bendavid <ben-david.alon@laplateforme.io>
2023-01-10 13:02:20 +0100

    working on to fetch pics for ech object

--- classes/cards.php
+++ classes/cards.php
@@ -1,12 +1,14 @@
 <?php
 class Card
 {
+    public $id;
     public $content;
     private $conn;
 
 
-    public function __construct($content)
+    public function __construct($content, $id)
     {
+        $this->id = $id;
         $this->content = $content;
         try {
             $this->conn = new PDO("mysql:host=localhost;dbname=memory_game", "root", "");
@@ -16,6 +18,11 @@ class Card
 
         return $this->conn;
     }
+    // public function __toString()
+    // {
+    //     return 'This is MyClass object';
+    // }
+
 
     public function display()
     {
@@ -31,15 +38,30 @@ class Card
         $stmt->execute();
 
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            echo '<button class="card">';
+            echo '<button class="card" >';
             // echo '<img src="' . $row['backPic'] . '">';
             echo '<img class="images" src="data:image/jpeg;base64,' . base64_encode($row['backPic']) . '"/>';
+            // echo $row['id']; 
 
 
             echo '</button>';
         }
     }
-    public function checkConnection()
+    public function displayPic()
     {
+        $query = "SELECT backPic FROM cards WHERE id = $this->id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            echo '<button class="card" >';
+            echo '<img class="images" src="data:image/jpeg;base64,' . base64_encode($row['backPic']) . '"/>';
+            echo '</button>';
+        }
+    }
+    public function getId()
+    {
+        return $this->id;
     }
 }
--- index.php
+++ index.php
@@ -2,21 +2,68 @@
 require_once("header.php");
 
 
-$card = new Card('<img src="..\media\frontPic.jpg" alt="Card image">');
-for ($pairs = 0; $pairs < 3 * 2; $pairs++) {
-    if ($pairs == 3) {
+// for ($pairs = 0; $pairs < 3 * 2; $pairs++) {
+//     if ($pairs == 3) {
 
-        $card->display();
-    } else {
-        $card->display();
-    }
+//         $card->display();
+//     } else {
+//         $card->display();
+//     }
+// }
+// $card->displayBack();
+// $card->listenClick();
+
+// $cardList = array();
+// for ($n = 1; $n <= 12; $n++) {
+//     // $cardList[] = array('card' . $n);
+//     array_push($cardList, 'card' . $n);
+// }
+// foreach ($cardList as $x) {
+//     print_r($x);
+// }
+
+// var_dump($card);
+// for ($i = 1; $i <= 12; $i++) {
+//     $objectName = 'card' . $i;
+//     $objectName = new Card('<img src="..\media\frontPic.jpg" alt="Card image">');
+// }
+// echo $card1;
+
+
+$cards = [];
+for ($i = 1; $i <= 12; $i++) {
+    $cards['card' . $i] = new Card('<img src="..\media\frontPic.jpg" alt="Card image">', $i);
 }
-$card->displayBack();
+foreach ($cards as $x) {
+    // print_r($x);
+}
+echo "The id of the card is : " . $cards['card9']->getId();
+// $cards['card3']->displayPic();
+// print_r($cards['card1']);
+// print_r($cards['card2']);
+
 
 ?>
 
 <body>
     <!-- <img src="/media/frontPic.jpg" alt=""> -->
+    <!-- <div class="     ">
+
+        <div class="thecard">
+
+            <div class="thefront">
+                <h1>Front of Card</h1>
+                <p>This is the front of the card. It contains important information. Please see overleaf for more details.</p>
+            </div>
+
+            <div class="theback">
+                <h1>Back of Card</h1>
+                <p>Your use of this site is subject to the terms and conditions governing this and all transactions.</p>
+                <button>Submit</button>
+            </div>
+
+        </div>
+    </div> -->
 
 </body>
 
